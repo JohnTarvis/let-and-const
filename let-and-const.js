0586a162d625b63a976fc5475878d6ea55075a12
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2020-12-16 11:05:43 -0500

    Update let-and-const.js

--- let-and-const.js
+++ let-and-const.js
@@ -1,1 +1,26 @@
 const PI = 3.14
+
+//What is the difference between var and let?
+/*
+var is scoped in a function and let is block scoped
+var can be redeclared and let cannot
+var is hoisted and let is not
+*/
+
+//What is the difference between var and const?
+/*
+var can be reasigned 
+const cannot
+*/
+
+//What is the difference between let and const?
+/*
+let can be reasigned
+const cannot
+*/
+
+//What is hoisting?
+/*
+variable declarations are read first before they are used in functions
+as if the variables had all been moved to the top of your code
+*/
